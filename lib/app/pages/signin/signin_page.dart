958f565fbdcd5ba1f5d38f37887b3bd02dbce010
Patrick Soares de souza silva <patrick.soares@iholdbank.digital>
2023-08-30 16:27:06 -0300

    scanqrcode

--- lib/app/pages/signin/signin_page.dart
+++ lib/app/pages/signin/signin_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:wallet_digital/app/pages/signin/devices/mobile.dart';
 import 'package:wallet_digital/app/pages/signin/devices/tablet.dart';
 
@@ -19,7 +20,17 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
+    // return MobileScanner(
+    //     // fit: BoxFit.contain,
+    //     controller: MobileScannerController(
+    //       detectionSpeed: DetectionSpeed.normal,
+    //       facing: CameraFacing.back,
+    //       torchEnabled: true,
+    //     ),
+    //     onDetect: (capture) {
 
+    //     },
+    //   );
     return WillPopScope(
       onWillPop: ()async=>false,
       child: const ResponsiveScreen(mobile:  SignInMobile(), tablet:  SignInTablet(), web:  SignInWeb()));
